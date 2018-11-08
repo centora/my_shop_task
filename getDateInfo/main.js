782585b00f6c540daa41ee4ef50d4dbb1bea580a
oksana_b <oksana_2186@mail.ru>
2018-11-08 23:32:49 +0200

    HW_9/Task1: Fixed code style

--- getDateInfo/main.js
+++ getDateInfo/main.js
@@ -1,8 +1,8 @@
 const fs = require('file-system');
 
-const dirName = "new-folder";
+const dirName = 'new-folder';
 const date = new Date();
-const dateInfo = 'Current date is: ' + date.toLocaleDateString();
+const dateInfo = `Current date is: ${date.toLocaleDateString()}`;
 
 fs.mkdir(dirName, function(err) {
   fs.writeFile(`${dirName}/dateInfo.txt`, dateInfo, function(err) {})
