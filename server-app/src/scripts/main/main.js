e42e7d67516a5ae01815478887778cdeacdd160e
Oksana Blonskaya <oksana.blonskaya@dataart.com>
2018-11-20 14:32:03 +0200

    SASS tasks [Mixins]: Set styles for 10 items using scss cycles

--- server-app/src/scripts/main/main.js
+++ server-app/src/scripts/main/main.js
@@ -3,6 +3,18 @@ const content = `<h2>Main Content</h2>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae, blanditiis consequatur delectus deserunt, eius error facilis minus porro quaerat quisquam repellendus sint tempora tempore velit? Consequuntur quaerat recusandae voluptatibus?</p>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae, blanditiis consequatur delectus deserunt, eius error facilis minus porro quaerat quisquam repellendus sint tempora tempore velit? Consequuntur quaerat recusandae voluptatibus?</p>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae, blanditiis consequatur delectus deserunt, eius error facilis minus porro quaerat quisquam repellendus sint tempora tempore velit? Consequuntur quaerat recusandae voluptatibus?</p>
+  <ul class="items-list">
+    <li class="item-1">1</li>
+    <li class="item-2">2</li>
+    <li class="item-3">3</li>
+    <li class="item-4">4</li>
+    <li class="item-5">5</li>
+    <li class="item-6">6</li>
+    <li class="item-7">7</li>
+    <li class="item-8">8</li>
+    <li class="item-9">9</li>
+    <li class="item-10">10</li>
+  </ul>
 `;
 
 require('./main.scss');
