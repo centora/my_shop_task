93f0e1336f904d7cd76f3e35d199ef5629f7adc2
oksana_b <oksana_2186@mail.ru>
2018-11-26 21:08:49 +0200

    Les-14: writing tests [0.0.3]

---
+++ server-app/src/test-tasks/main.test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { showMessage, getDay, getAdultUsers, getRandomUsers, Product,  } from './main';
+/*
+
+describe('class Product', () => {
+
+  let product;
+
+  beforeEach(() => {
+    product = new Product();
+  });
+
+  it('should have default title', () => {
+    expect(product.title).to.equal('Apple');
+  });
+
+  it('should have default price', () => {
+    expect(product.price).to.equal(10);
+  });
+
+  it("should get price",  () => {
+    
+  });
+
+});*/
