8225c76d63d532e8e3f043a2b7459f5e64e2355f
oksana_b <oksana_2186@mail.ru>
2018-12-03 23:17:54 +0200

    Fixed errors using lint.

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,17 +1,25 @@
-import React from 'react';
 import { Aside } from '../aside';
 import './main.scss';
 
 export const Main = () => (
   <>
     <main className="main">
-      <Aside></Aside>
+      <Aside />
       <div className="content">
         <h3>Lorem ipsum dolor sit amet</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda at culpa cum cupiditate earum est ipsa minima molestias neque nisi officia, officiis omnis pariatur porro sint sit tenetur.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda at culpa cum cupiditate earum est ipsa minima molestias neque nisi officia, officiis omnis pariatur porro sint sit tenetur.</p>
+        <p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda at culpa cum cupiditate
+          earum est ipsa minima molestias neque nisi officia, officiis omnis pariatur porro sint sit tenetur.
+        </p>
+        <p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda at culpa cum cupiditate
+          earum est ipsa minima molestias neque nisi officia, officiis omnis pariatur porro sint sit tenetur.
+        </p>
         <h3>Lorem ipsum dolor sit amet</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda at culpa cum cupiditate earum est ipsa minima molestias neque nisi officia, officiis omnis pariatur porro sint sit tenetur.</p>
+        <p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda at culpa cum cupiditate
+          earum est ipsa minima molestias neque nisi officia, officiis omnis pariatur porro sint sit tenetur.
+        </p>
       </div>
     </main>
   </>
