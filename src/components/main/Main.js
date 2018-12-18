b3cb0cc819a41111d7cee5fd24e0a84ee7f94251
oksana_b <oksana_2186@mail.ru>
2018-12-18 10:52:38 +0200

    HW-18: Created Todo List component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,15 +3,37 @@ import './main.scss';
 import { EditableText } from '../editableText';
 
 export class Main extends Component {
+  state = {
+    data: ''
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        data: {
+          email: 'oksa@oksa.com',
+          name: 'Oksana',
+          surname: 'Oksa'
+        }
+      });
+    }, 1000);
+  }
+
+  onSave = (data) => {
+    console.log(data);
+  }
+
   render() {
     const { title } = this.props;
+    const { data } = this.state;
     const fn = text => console.log(text);
+
     return (
       <main className="main">
         <div className="content">
           <h1>{title}</h1>
           <EditableText onUpdateFieldHandle={fn} />
-          <UserForm />
+          <UserForm data={data} onSave={this.onSave} />
         </div>
       </main>
     );
--- src/components/userForm/UserForm.js
+++ src/components/userForm/UserForm.js
@@ -3,8 +3,23 @@ import './userForm.scss';
 export class UserForm extends Component {
   roles = ['Admin', 'User', 'Guest'];
 
+  static getDerivedStateFromProps({ data }, currentState) {
+    if (!data) return null;
+    const props = Object.entries(data);
+    const isStateEmpty = props.every(([key]) => !currentState[key].value);
+
+    if (isStateEmpty) {
+      const state = {};
+      props
+        .forEach(([key, value]) => state[key] = { value, error: '' });
+      return state;
+    }
+    return null;
+  }
+
   constructor(props) {
     super(props);
+
     this.fields = [
       { label: 'email', reg: /^\w+@\w+\.[a-z]{2,}$/, placeholder: 'Enter your email' },
       { label: 'name', reg: /^[^ ]{3,20}$/, placeholder: 'Enter your name' },
@@ -19,35 +34,80 @@ export class UserForm extends Component {
 
     this.state = {};
 
-    this.fields.forEach(({ label }) => this.state[label] = '');
+    this.fields.forEach(({ label }) => this.state[label] = ({
+      value: '',
+      error: ''
+    }));
   }
 
   onChange = ({ target }) => {
+    const field = this.state[target.name];
     if (/checkbox|radio/i.test(target.type)) {
-      this.setState({ [target.name]: target.checked });
+      this.setState({ [target.name]: { ...field, value: target.checked } });
     } else {
-      this.setState({ [target.name]: target.value });
+      this.setState({ [target.name]: { ...field, value: target.value } });
     }
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const data = {};
+    Object.entries(this.state)
+      .forEach(([key, { value }]) => data[key] = value);
+    if (this.props.onSave) {
+      this.props.onSave(data);
+    }
+  }
+
+  onValidate = ({ target }) => {
+    const field = this.fields.find(item => item.label === target.name);
+    const stateField = this.state[target.name];
+    let error = '';
+
+    if (!target.value || !field.reg.test(target.value)) {
+      error = 'Is not valid';
+    } else {
+      error = '';
+    }
+
+    if (target.name === 'repeatPassword' && this.state.password.value !== target.value) {
+      error = 'Password are not equaled';
+    }
+
+    this.setState({
+      [target.name]: { ...stateField, error }
+    });
+  }
+
+  isButtonDisabled() {
+    return Object.entries(this.state).some(([key, { error, value }]) => error || !value);
   }
 
   render() {
     return (
-      <form action="" className="form" autoComplete="off">
+      <form action="" className="form" autoComplete="off" onSubmit={this.onSubmit}>
         <div>
           {
-            this.fields.map((field, id) => (
-              <div key={id}>
+            this.fields.map((field, index) => (
+              <div key={index} className="form-field">
                 <input
                   type={field.secure ? 'password' : 'text'}
                   name={field.label}
-                  value={this.state[field.label]}
+                  value={this.state[field.label].value}
                   placeholder={field.placeholder}
+                  onBlur={this.onValidate}
                   onChange={this.onChange}
                 />
+                {
+                  this.state[field.label].error
+                  && <mark>{this.state[field.label].error}</mark>
+                }
               </div>
             ))
           }
         </div>
+        <br />
+        <button type="submit" disabled={this.isButtonDisabled()}>Send</button>
       </form>
     );
   }
