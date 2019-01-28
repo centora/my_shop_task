9d569c47b05bfbf5623bb45f0171581a8cd1ae44
oksana_b <oksana_2186@mail.ru>
2019-01-29 01:43:12 +0200

    Fixed header rendering

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,38 +1,36 @@
 import { Link } from 'react-router-dom';
 import { Nav } from '../navigation';
-import { logout } from '../../services';
 import './header.scss';
 
 export const Header = ({
   user,
   info,
-  onLogout,
-  history
+  onLogout
 }) => {
-  const onLogoutHandler = (event) => {
-    event.preventDefault();
-    logout().then(() => {
-      onLogout();
-      history.push('/');
-    });
+  const onLogoutHandler = () => {
+    onLogout();
   };
-  return (
 
+  const RenderUserBox = ({ user, info }) => {
+    const { firstName } = user;
+    return (<>
+      <Link to="/profile">
+        <span>{firstName}</span> {info && `(${info.categories}/${info.products})`}
+      </Link> &nbsp; &nbsp;
+      <span><a href="#" onClick={onLogoutHandler}>Logout</a></span>
+    </>);
+  }
+
+  return (
     <header className="header">
       <Link to="/" className="logo">
         <img src="/images/logo.png" alt="Todo" />
       </Link>
       <Nav list={['Home', user ? 'Categories' : 'Shop', user ? 'Products' : 'Contacts']} />
       <div className="user-box">
-        { user ? (
-          <>
-            <Link to="/profile">
-              <span>{user.firstName}</span> {info && `(${info.categories}/${info.products})`}
-            </Link> &nbsp; &nbsp;
-            <span><a href="#" onClick={onLogoutHandler}>Logout</a></span>
-          </>
-        ) : <Nav list={['Signin', 'Signup']} />
-}
+        {
+          user ? <RenderUserBox user={user} info={info} /> : <Nav list={['Signin', 'Signup']} />
+        }
       </div>
     </header>
   );
